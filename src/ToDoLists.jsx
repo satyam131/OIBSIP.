e672b3dd88b914a27c1fc3b99f9f2a72f7823867
Satyam Kumar Prajapati <73646662+satyam131@users.noreply.github.com>
2022-07-21 13:17:17 +0530

    Add files via upload

---
+++ src/ToDoLists.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ToDoLists = (props) => {
+
+    
+
+    return(
+        <>
+        <div className="todo_style">
+            <i className="fa fa-times" onClick={() => {
+                props.onSelect(props.id);
+            }} > X </i>
+            <li> {props.text} </li>
+        </div>
+        </>
+    );
+};
+
+export default ToDoLists;
